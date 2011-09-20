1496ba54f9027ed55b089825774761b2e9027221
Dexter Winner <dexter.winner@gmail.com>
2011-09-20 15:06:55 +0300

    create table

--- univer/models.py
+++ univer/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-#Making table for "Student"
 class Student(models.Model):
 	last_name = models.CharField(max_lenght=40)
 	first_name = models.CharField(max_lenght=30)
@@ -8,8 +7,7 @@ class Student(models.Model):
 	birthday = models.DateField()
 	stud_card = models.CharField(max_lenght=10)
 	group = models.ForeignKey(Group)
-
-#Making table for "Group"	
+	
 class Group(models.Model):
 	name = models.CharField(max_lenght=10)
 	warden = models.ForeignKey(Student)
