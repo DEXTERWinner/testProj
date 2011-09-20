70ecacb365ba6bb620d8c01b0dbfb621090620bb
Dexter Winner <dexter.winner@gmail.com>
2011-09-20 15:31:51 +0300

    commit

--- univer/models.py
+++ univer/models.py
@@ -7,9 +7,9 @@ class Student(models.Model):
 	middle_name = models.CharField(max_length=40)
 	birthday = models.DateField()
 	stud_card = models.CharField(max_length=10)
-	group = models.ForeignKey(Group)
+	group = models.ForeignKey('Group')
 	
 	
 class Group(models.Model):
 	name = models.CharField(max_length=10)
-	warden = models.ForeignKey(Student)
+	warden = models.ForeignKey(Student, related_name='warden')
